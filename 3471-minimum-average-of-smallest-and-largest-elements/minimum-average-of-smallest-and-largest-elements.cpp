85e30e9d1868705b0dcff8c7780ce59d208393a2
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-06-23 12:30:03 +0530

    Time: 3 ms (71.43%) | Memory: 25.2 MB (28.57%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     double minimumAverage(vector<int>& nums) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
         int n = nums.size();
         vector<double> avg;
         sort(nums.begin(), nums.end());
