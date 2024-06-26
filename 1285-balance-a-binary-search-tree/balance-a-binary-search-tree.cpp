fa5bf5a11a92c9871dad40f00cf662fd41378657
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-06-26 21:01:12 +0530

    Time: 75 ms (84.05%) | Memory: 62.8 MB (26.28%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -12,6 +12,9 @@
 class Solution {
 public:
     void inorder(TreeNode* root, vector<int>& vals) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         if (!root) return;
         inorder(root->left, vals);
         vals.push_back(root->val);
@@ -19,6 +22,9 @@ public:
     }
     
     TreeNode* build(const vector<int>& vals, int l, int r) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         if (l > r) return nullptr;
         int m = l + (r - l) / 2;
         TreeNode* node = new TreeNode(vals[m]);
@@ -26,8 +32,11 @@ public:
         node->right = build(vals, m + 1, r);
         return node;
     }
-    
+
     TreeNode* balanceBST(TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         vector<int> vals;
         inorder(root, vals);
         return build(vals, 0, vals.size() - 1);
