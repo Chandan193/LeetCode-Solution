bd3550441dc137e90b78c13d1fe5cea1ab913f11
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-06-29 00:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (25.30%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
         int xorr = 0;
         for(int i=0; i<nums.size(); i++){
             xorr ^= nums[i];
