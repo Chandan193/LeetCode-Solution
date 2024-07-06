4a24df3132011b82ef41120020e8b89543d8ca79
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-07-06 15:35:51 +0530

    Time: 24 ms (16.70%) | Memory: 11.3 MB (34.17%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+         ios_base::sync_with_stdio(false);
+         cin.tie(NULL);
+
         map<char, int> mp;
         for(char ch : magazine){
             mp[ch]++;
