1185f3c06d733575ffe9b239c2ef3a8cd614bc79
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-07-12 23:51:01 +0530

    Time: 115 ms (24.80%) | Memory: 27.1 MB (27.64%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-         if (x < y) {
-        swap(x, y);
+    if (x < y) {
+    swap(x, y);
         for (char &c : s) {
-            if (c == 'a') c = 'b';
-            else if (c == 'b') c = 'a';
+        if (c == 'a') c = 'b';
+        else if (c == 'b') c = 'a';
         }
     }
 
     int score = 0;
     stack<char> st;
 
-    // Remove all "ab" pairs first
     for (char c : s) {
         if (!st.empty() && st.top() == 'a' && c == 'b') {
             st.pop();
@@ -22,14 +21,15 @@ public:
         }
     }
 
-    string remaining;
+    string rem;
     while (!st.empty()) {
-        remaining += st.top();
+        rem += st.top();
         st.pop();
     }
-    reverse(remaining.begin(), remaining.end());
+    
+    reverse(rem.begin(), rem.end());
 
-    for (char c : remaining) {
+    for (char c : rem) {
         if (!st.empty() && st.top() == 'b' && c == 'a') {
             st.pop();
             score += y;
