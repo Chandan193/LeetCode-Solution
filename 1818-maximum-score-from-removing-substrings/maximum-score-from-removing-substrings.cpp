ec725c7d2e9e6b16cd68f4e5622090c79f88fef7
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-07-12 23:56:12 +0530

    Time: 82 ms (63.42%) | Memory: 27.3 MB (25.61%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,40 +1,62 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-    if (x < y) {
-    swap(x, y);
-        for (char &c : s) {
-        if (c == 'a') c = 'b';
-        else if (c == 'b') c = 'a';
-        }
-    }
-
-    int score = 0;
-    stack<char> st;
-
-    for (char c : s) {
-        if (!st.empty() && st.top() == 'a' && c == 'b') {
-            st.pop();
-            score += x;
-        } else {
-            st.push(c);
-        }
-    }
-
-    string rem;
-    while (!st.empty()) {
-        rem += st.top();
-        st.pop();
-    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
-    reverse(rem.begin(), rem.end());
+    int score = 0;
 
-    for (char c : rem) {
-        if (!st.empty() && st.top() == 'b' && c == 'a') {
-            st.pop();
-            score += y;
-        } else {
-            st.push(c);
+    if (x > y) {
+        stack<char> stk;
+        for (char c : s) {
+            if (!stk.empty() && stk.top() == 'a' && c == 'b') {
+                stk.pop();
+                score += x;
+            } else {
+                stk.push(c);
+            }
+        }
+
+        string rem;
+        while (!stk.empty()) {
+            rem += stk.top();
+            stk.pop();
+        }
+        reverse(rem.begin(), rem.end());
+
+        for (char c : rem) {
+            if (!stk.empty() && stk.top() == 'b' && c == 'a') {
+                stk.pop();
+                score += y;
+            } else {
+                stk.push(c);
+            }
+        }
+    } else {
+        stack<char> stk;
+        for (char c : s) {
+            if (!stk.empty() && stk.top() == 'b' && c == 'a') {
+                stk.pop();
+                score += y;
+            } else {
+                stk.push(c);
+            }
+        }
+
+        string rem;
+        while (!stk.empty()) {
+            rem += stk.top();
+            stk.pop();
+        }
+        reverse(rem.begin(), rem.end());
+
+        for (char c : rem) {
+            if (!stk.empty() && stk.top() == 'a' && c == 'b') {
+                stk.pop();
+                score += x;
+            } else {
+                stk.push(c);
+            }
         }
     }
 
