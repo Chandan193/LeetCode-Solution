4a70286bc93722805df033ceedc0ef2b8259987e
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-07-15 00:55:20 +0530

    Time: 148 ms (48.16%) | Memory: 47.4 MB (25.26%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int partitionString(string s) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
         int ans = 0;
         unordered_set<char> seen;
         for(char &c : s){
