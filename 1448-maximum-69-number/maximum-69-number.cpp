a37748bcaad7122b37addb13c87a4c11545ee624
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-07-15 00:30:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (7.72%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string s = to_string(num);
-        for(int i=0; i<s.length(); i++){
-            if(s[i]=='6'){
-                s[i] = '9';
+        for(char &c : s){
+            if(c=='6'){
+                c = '9';
                 break;
             }
         }
