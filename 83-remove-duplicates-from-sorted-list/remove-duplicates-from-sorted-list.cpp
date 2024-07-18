6f692aa23e05a3a9d68971ef10679028ae2ce77f
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-07-18 23:27:19 +0530

    Time: 3 ms (94.45%) | Memory: 17.8 MB (6.59%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,6 +11,9 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         if(head == nullptr) return head;
 
        ListNode* curr = head;
