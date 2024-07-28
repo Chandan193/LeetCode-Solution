c15118a726f18915e6dde8f227c000407fe36374
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-07-28 10:05:12 +0530

    Time: 8 ms (33.33%) | Memory: 28.2 MB (16.67%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canAliceWin(vector<int>& nums) {
       int sd = 0, dd = 0;
-      for(int n : nums){
+      for(auto n : nums){
         if(n<=9) sd += n;
         else dd += n;
       }  
