e113d86acb8abb8461b969693116a2db7c4913cf
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-08-01 11:00:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -3,7 +3,9 @@ public:
     int countSeniors(vector<string>& details) {
         int cnt = 0;
         for(int i=0; i<details.size(); i++){
-            cnt += (10*(details[i][11]-'0') + (details[i][12]-'0')) > 60;
+            if((10*(details[i][11]-'0') + (details[i][12]-'0')) > 60){
+                cnt++;
+            }
         }
         return cnt;
     }
