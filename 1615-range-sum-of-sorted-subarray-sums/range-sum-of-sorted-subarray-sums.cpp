7f29b46b25aa56a30b54996943a70fc3bf8e594d
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-08-05 00:57:54 +0530

    Time: 94 ms (45.62%) | Memory: 27.1 MB (39.10%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     const int MOD = 1000000007;
-    int sum(vector<int>& nums, int i, int j) {
+    int sum(const vector<int>& nums, int i, int j) {
     int sum = 0;
     for (int k = i; k <= j; k++) {
         sum += nums[k];
@@ -10,18 +10,23 @@ public:
     }
 
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-    long long total = 0;
     vector<int> arr;
+    
     for (int i = 0; i < nums.size(); i++) {
+        int curr = 0;
         for (int j = i; j < nums.size(); j++) {
-            arr.push_back(sum(nums, i, j));
+            curr += nums[j];
+            arr.push_back(curr);
         }
     }
-    sort(arr.begin(), arr.end());
 
+    sort(arr.begin(), arr.end());
+    
+    long long total = 0;
     for (int i = left - 1; i <= right - 1; i++) {
         total = (total + arr[i]) % MOD;
     }
-    return (int)total;
-    }
+
+    return (int)(total);
+}
 };
