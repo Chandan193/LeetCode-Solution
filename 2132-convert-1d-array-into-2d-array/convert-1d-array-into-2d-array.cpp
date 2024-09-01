459ddec1f5e0a7784359e3a6675334b77f822c06
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-09-01 20:12:04 +0530

    Time: 75 ms (81.53%) | Memory: 88.2 MB (55.59%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-    vector<int> ins(vector<int>& o, int start, int end){
-        vector<int> temp;
-        for(int i=start; i<end; i++){
-            temp.push_back(o[i]);
-        }
-        return temp;
-    }
-    vector<vector<int>> construct2DArray(vector<int>& o, int m, int n) {
-        int size = o.size();
-        if(m*n!= size) return {};
-
-        vector<vector<int>> ans;
-
-        int start = 0, end = n;
-
-        int p1 = 0, p2 = 1;
-
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(original.size() != m*n) return {};
+        vector<vector<int>> ans(m,vector<int>(n));
+        int idx = 0;
         for(int i=0; i<m; i++){
-            start = p1*n;
-            end = p2*n;
-
-            p1 += 1;
-            p2 += 1;
-
-            ans.push_back(ins(o, start, end));
+            for(int j=0; j<n; j++){
+                ans[i][j] = original[idx++];
+            }
         }
         return ans;
     }
