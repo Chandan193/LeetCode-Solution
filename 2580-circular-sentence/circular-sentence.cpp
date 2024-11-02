492a19d03bbcfe091244584f9a32a942c8475b86
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2024-11-02 09:13:34 +0530

    Time: 7 ms (5.45%) | Memory: 9.8 MB (6.09%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        string temp = "";
+        vector<string> arr;
+        for(char c : s){
+            if(c!=' ')temp += c;
+            if(c==' '){
+                arr.push_back(temp);
+                temp = "";
+            }
+        }
+
+        if(temp.length()>0) arr.push_back(temp);
+
+        for(string i : arr) cout<<i<<" ";
+
+        if(arr.size()==1){
+            string t = arr[0];
+            if(t[0] == t.back()) return true;
+            else return false;
+        }
+
+        string t1 = arr[0];
+        string t2 = arr.back();
+
+        if(t1[0] != t2.back()) return false;
+
+        for(int i=1; i<arr.size(); i++){
+            string t1 = arr[i];
+            string t2 = arr[i-1];
+            if(t1[0] != t2.back()) return false;
+        }
+
+        return true;
+    }
+};
