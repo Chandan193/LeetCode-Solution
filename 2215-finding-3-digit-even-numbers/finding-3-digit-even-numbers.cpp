40bc06db5b5a9c3da0366c2103cfc01ebbca4b23
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2025-05-12 19:18:08 +0530

    Time: 283 ms (24.23%) | Memory: 14.3 MB (36.26%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     void fun(int x, int y, int z, set<int>& st){
         int n1, n2, n3, n4, n5, n6;
+        if(x%2==0){
         n1 = x + (y*10) + (z*100);
+        if(n1>=100) st.insert(n1);
         n2 = x + (z*10) + (y*100);
+        if(n2>=100) st.insert(n2);
+        }
+
+        if(y%2==0){
         n3 = y + (x*10) + (z*100);
+        if(n3>=100) st.insert(n3);
         n4 = y + (z*10) + (x*100);
+        if(n4>=100) st.insert(n4);
+        }
+
+        if(z%2==0){
         n5 = z + (x*10) + (y*100);
+        if(n5>=100) st.insert(n5);
         n6 = z + (y*10) + (x*100);
-
-        if(n1>=100 && n1%2==0) st.insert(n1);
-        if(n2>=100 && n2%2==0) st.insert(n2);
-        if(n3>=100 && n3%2==0) st.insert(n3);
-        if(n4>=100 && n4%2==0) st.insert(n4);
-        if(n5>=100 && n5%2==0) st.insert(n5);
-        if(n6>=100 && n6%2==0) st.insert(n6);
-
+        if(n6>=100) st.insert(n6);
+        }
     }
     vector<int> findEvenNumbers(vector<int>& digits) {
         set<int> st;
