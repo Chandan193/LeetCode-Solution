30191412e18defe0df4e2b11c9387f436f4317cb
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2025-06-26 17:19:42 +0530

    Time: 3 ms (68.32%) | Memory: 67.5 MB (24.34%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int check(vector<int>& nums, int mid){
-        int cnt = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] <= mid) cnt++;
-        }
-        return cnt;
-    }
     int findDuplicate(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
         int n = nums.size();
-        int left = 0, right = n - 1;
-
-        while(left<=right){
-            int mid = (left + right)/2;
-            if(check(nums, mid) > mid){
-                right = mid - 1;
+        vector<int> freq(n+1, 0);
+        for(int i=0; i<n; i++){
+            if(freq[nums[i]]==0){
+                freq[nums[i]] = 1;
             }
             else{
-                left = mid + 1;
+                return nums[i];
             }
         }
-        return left;
+        return -1;
     }
 };
