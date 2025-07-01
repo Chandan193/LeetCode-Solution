407f7aef8aa424e5b7d33f2313dd3a21a3cdcadf
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2025-07-01 20:51:39 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (86.06%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        int N = m + n - 2;
-        int r = m - 1;
+    int nCr(int n, int r){
         double ans = 1;
-        for(int i=1; i<=r; i++){
-            ans = ans * (N - r + i)/i;
+        for(int i=0; i<r; i++){
+            ans *= (n-i);
+            ans /= (i+1);
         }
-        return int(ans);
+        return (int)ans;
+    }
+    int uniquePaths(int m, int n) {
+        int N = (m+n)-2;
+        int r = min(m, n)-1;
+        return nCr(N, r);
     }
 };
