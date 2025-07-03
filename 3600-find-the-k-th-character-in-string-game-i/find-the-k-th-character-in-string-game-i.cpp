41469a6eacc661bc81b55ed7000b0b2ef872461a
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2025-07-03 23:51:01 +0530

    Time: 3 ms (57.81%) | Memory: 9.4 MB (68.80%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string word = "a";
-        string temp = "";
+        string word = "a", temp = "";
         while(word.length() < k){
-            for(int i=0; i<word.length(); i++){
-                temp += (word[i]+1);
-            }
+            for(int i=0; i<word.length(); i++)  temp += (word[i]+1);
             word = word+temp;
-            // cout<<word<<" ";
             temp = "";
         }
         return word[k-1];
