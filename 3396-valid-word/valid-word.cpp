4d44df4a4a53dd268198dfcb670541f49a4e39c9
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2025-07-15 09:17:01 +0530

    Time: 2 ms (13.35%) | Memory: 8.8 MB (5.83%) - LeetSync

---
+++ 3396-valid-word/valid-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string word) {
+       if(word.length()<3) return false;
+       int v = 0, c = 0, d = 0;
+       unordered_set<char> st = {'a','e','i','o','u','A','E','I','O','U'};
+       for(char x : word){
+        if((x>='a' && x<='z') || (x>='A' && x<='Z')){
+            if(st.find(x)!=st.end()) v++;
+            else c++;
+        }
+        else if(x>='0' && x<='9') d++;
+       }
+       return (v>=1 && c>=1 && (v+c+d)==word.size());
+    }
+};
