d2aaa20c5c3a21cf91891a18334a7e70044bd2bb
Chandan Ghosh <96768162+Chandan193@users.noreply.github.com>
2025-08-05 11:57:32 +0530

    Time: 4 ms (35.77%) | Memory: 49.9 MB (17.95%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -3,20 +3,19 @@ public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n = baskets.size();
         vector<int> avail(n, 1);
+        int cnt = 0;
         for(int i=0; i<fruits.size(); i++){
             int f = fruits[i];
             for(int j=0; j<n; j++){
                 if(baskets[j]>=f && avail[j]==1){
                     avail[j] = 0;
+                    cnt++;
                     break;
                 }
             }
         }
 
-        int cnt = 0;
 
-        for(int x : avail) cnt += (x==1) ? 1 : 0;
-
-        return cnt;
+        return (avail.size()-cnt);
     }
 };
